868ab640f534f05348148eff899cfba6d9cf6280
Tom Mullier <tom.mullier@outlook.fr>
2023-01-11 10:50:17 +0100

    modal on scan

--- Code/Vue/JS/user_route_list.js
+++ Code/Vue/JS/user_route_list.js
@@ -1,36 +1,63 @@
 let all_desc = document.getElementsByClassName("route_element_desc_text")
 let all_expand_buttons = document.getElementsByClassName("expand_button")
-all_desc=Array.from(all_desc)
-all_expand_buttons=Array.from(all_expand_buttons)
+all_desc = Array.from(all_desc)
+all_expand_buttons = Array.from(all_expand_buttons)
 
 all_expand_buttons.forEach(function (element) {
         element.addEventListener("click", function (e) {
                 console.log(e.target)
-                all_desc.forEach(function (el){
-                        el.style.overflow="hidden"
-                        el.style.lineClamp=3;
-                        el.style.display="-webkit-box";
+                all_desc.forEach(function (el) {
+                        el.style.overflow = "hidden"
+                        el.style.lineClamp = 3;
+                        el.style.display = "-webkit-box";
 
                 })
                 let desc = e.target.parentElement.parentElement.getElementsByClassName("route_element_desc_text")[0]
-                desc.style.overflow="visible";
-                desc.style.lineClamp=999999;
-                desc.style.display="flex";
+                desc.style.overflow = "visible";
+                desc.style.lineClamp = 999999;
+                desc.style.display = "flex";
         });
 })
 
 
-let button_sort=document.getElementById("sort_button")
-let sort_menu=document.getElementById("sort_menu")
+let button_sort = document.getElementById("sort_button")
+let sort_menu = document.getElementById("sort_menu")
 button_sort.addEventListener("click", function (e) {
-        if (sort_menu.style.display=="flex") {
-                sort_menu.style.display="none";
+        if (sort_menu.style.display == "flex") {
+                sort_menu.style.display = "none";
                 return;
         } else {
-                sort_menu.style.display="flex";
-                document.getElementById("scroll_list").style.height="calc(84vh - 40px)";
+                sort_menu.style.display = "flex";
+                document.getElementById("scroll_list").style.height = "calc(84vh - 40px)";
         }
 });
+
+let sortTitle = []
+let listItems = document.getElementsByClassName("titles")
+listItems = Array.from(listItems);
+let sortList=document.getElementsByClassName('filtre_container');
+sortList=Array.from(sortList);
+sortList.forEach(function (element) {
+        element.addEventListener('click', function (e) {
+                console.log(element)
+                if(element.Id('sort-title') == true){
+                        for (let i = 0; i < listItems.length; i++){
+                                sortTitle[i] =  listItems[i].innerText;
+                        }       
+                        sortList.sort();
+                        console.log(sortList);
+                }
+                else if(element.getElementById('sort-duration') == true){
+
+                }
+                else if(element.getElementById('sort-author') == true){
+
+                }
+
+        });
+});
+
+
 
 
 
@@ -39,9 +66,6 @@ document.getElementById('searchBar').addEventListener('input', filterList);
 function filterList() {
         const searchBar = document.getElementById('searchBar');
         const filter = searchBar.value.toLowerCase();
-
-
-        console.log(filter);
 
         let listItems = document.getElementsByClassName("titles")
         let auteurs = document.getElementsByClassName('auteur');
