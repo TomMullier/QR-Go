ee1fb286babc7d80f1d81c269a3ada44e3af0124
Tom Mullier <tom.mullier@outlook.fr>
2023-01-11 12:58:49 +0100

    change vh in %

--- Code/Vue/JS/user_route_list.js
+++ Code/Vue/JS/user_route_list.js
@@ -5,7 +5,8 @@ all_expand_buttons = Array.from(all_expand_buttons)
 
 all_expand_buttons.forEach(function (element) {
         element.addEventListener("click", function (e) {
-                console.log(e.target)
+
+                console.log(e)
                 all_desc.forEach(function (el) {
                         el.style.overflow = "hidden"
                         el.style.lineClamp = 3;
@@ -16,8 +17,41 @@ all_expand_buttons.forEach(function (element) {
                 desc.style.overflow = "visible";
                 desc.style.lineClamp = 999999;
                 desc.style.display = "flex";
+
         });
 })
+
+
+//get name, desc and duraction from modal
+let route_title = document.getElementById("title_txt")
+let route_desc = document.getElementById("route_desc")
+let route_duration = document.getElementById("route_duration")
+// let route_title=document.getElementById("title_txt")
+
+let allCards = document.getElementsByClassName("route-element")
+allCards = Array.from(allCards)
+console.log(allCards)
+
+allCards.forEach(function (element) {
+        element.addEventListener("click", function (e) {
+                if (!e.target.classList.contains("expand_button")) {
+                        route_title.innerText = element.getElementsByClassName("titles")[0].innerText
+                        route_desc.innerText = element.getElementsByClassName("route_element_desc_text")[0].innerText
+                        route_duration.value = element.getElementsByClassName("duration")[0].innerText
+                        //focus curor on nothin
+                        
+                        modals.show("display_route_modal");
+                        if (document.activeElement != document.body) document.activeElement.blur();
+                }
+
+        });
+})
+
+
+
+
+
+
 
 
 let button_sort = document.getElementById("sort_button")
@@ -28,7 +62,7 @@ button_sort.addEventListener("click", function (e) {
                 return;
         } else {
                 sort_menu.style.display = "flex";
-                document.getElementById("scroll_list").style.height = "calc(84vh - 40px)";
+                document.getElementById("scroll_list").style.height = "calc(68% - 40px)";
         }
 });
 
